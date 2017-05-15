f63afe7843afc50c82ee156796443b17abe01ece
Denys Nosov <denys@joomla-ua.org>
2017-05-15 11:54:51 +0300

    Update installer script

--- script.php
+++ script.php
@@ -29,43 +29,31 @@ jimport('joomla.filesystem.folder');
 
 class com_JUPollsInstallerScript
 {
-    private $_current_version = null;
     private $_is_new_installation = true;
 
     public function preflight($type, $parent)
     {
         $app = JFactory::getApplication();
+        $db  = JFactory::getDbo();
 
-        /*
-        $folders = array(
-            JPATH_SITE . '/administrator/components/com_jupolls',
-            JPATH_SITE . '/components/com_jupolls',
-            JPATH_SITE . '/modules/mod_jupolls',
-            JPATH_SITE . '/plugins/jupolls',
-            JPATH_SITE . '/media/jupolls'
-        );
-
-        foreach ($folders AS $folder)
+        if(!file_exists(JPATH_ROOT . '/images/polls'))
         {
-            if(is_dir($folder))
+            if(JFolder::create(JPATH_ROOT . '/images/polls'))
             {
-                $this->unlinkRecursive($folder, 1);
-                $app->enqueueMessage('Delete folder: ' . $folder, 'message');
+                $app->enqueueMessage('Create folder: polls', 'message');
+            }
+            else
+            {
+                $app->enqueueMessage('Unable to create folder: polls', 'message');
             }
         }
-        */
 
-        if(JFolder::create(JPATH_ROOT . '/images/polls'))
-        {
-            $app->enqueueMessage('Create folder: polls', 'message');
-        }
-        else
-        {
-            $app->enqueueMessage('Unable to create folder: polls', 'message');
-        }
-
-        $db = JFactory::getDBO();
-        $db->setQuery('SELECT params FROM #__extensions WHERE element = "com_jupolls" AND type = "component"');
+        $query = $db->getQuery(true);
+        $query->select('params');
+        $query->from('#__extensions');
+        $query->where($db->quoteName('element') . ' = ' . $db->quote('com_jupolls'));
+        $query->where($db->quoteName('type') . ' = ' . $db->quote('component'));
+        $db->setQuery($query);
         $config = $db->loadResult();
 
         if(!empty($config))
@@ -78,49 +66,52 @@ class com_JUPollsInstallerScript
     {
         $status = new JObject();
         $app    = JFactory::getApplication();
+        $db     = JFactory::getDBO();
 
-        $db  = JFactory::getDBO();
+        if($this->_is_new_installation == true)
+        {
+            $db->setQuery("CREATE TABLE IF NOT EXISTS `#__jupolls_polls` (
+              `id` int(11) unsigned NOT NULL auto_increment,
+              `title` varchar(255) NOT NULL default '',
+              `alias` varchar(255) NOT NULL default '',
+              `checked_out` int(11) NOT NULL default '0',
+              `checked_out_time` datetime NOT NULL default '0000-00-00 00:00:00',
+              `published` tinyint(1) NOT NULL default '0',
+              `publish_up` datetime NOT NULL default '0000-00-00 00:00:00',
+              `publish_down` datetime default '0000-00-00 00:00:00',
+              `params` text NOT NULL,
+              `access` int(11) NOT NULL default '0',
+              `lag` int(11) NOT NULL default '0',
+              PRIMARY KEY  (`id`)
+            ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=0;");
+            $db->execute();
+
+            $db->setQuery("CREATE TABLE IF NOT EXISTS `#__jupolls_options` (
+              `id` int(11) NOT NULL auto_increment,
+              `poll_id` int(11) NOT NULL default '0',
+              `text` text NOT NULL,
+              `link` varchar(255) DEFAULT NULL,
+              `ordering` int(11) NOT NULL,
+              PRIMARY KEY  (`id`),
+              KEY `poll_id` (`poll_id`,`text`(1))
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=0;");
+            $db->execute();
+
+            $db->setQuery("CREATE TABLE IF NOT EXISTS `#__jupolls_votes` (
+              `id` bigint(20) NOT NULL auto_increment,
+              `date` datetime NOT NULL default '0000-00-00 00:00:00',
+              `option_id` int(11) NOT NULL default '0',
+              `poll_id` int(11) NOT NULL default '0',
+              `ip` int(10) unsigned NOT NULL,
+              `browser` varchar(155) NOT NULL,
+              `user_id` int(11) DEFAULT NULL,
+              PRIMARY KEY  (`id`),
+              KEY `poll_id` (`poll_id`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=0;");
+            $db->execute();
+        }
+
         $src = $parent->getParent()->getPath('source');
-
-        $db->setQuery("CREATE TABLE IF NOT EXISTS `#__jupolls_polls` (
-		  `id` int(11) unsigned NOT NULL auto_increment,
-		  `title` varchar(255) NOT NULL default '',
-		  `alias` varchar(255) NOT NULL default '',
-		  `checked_out` int(11) NOT NULL default '0',
-		  `checked_out_time` datetime NOT NULL default '0000-00-00 00:00:00',
-		  `published` tinyint(1) NOT NULL default '0',
-		  `publish_up` datetime NOT NULL default '0000-00-00 00:00:00',
-		  `publish_down` datetime default '0000-00-00 00:00:00',
-		  `params` text NOT NULL,
-		  `access` int(11) NOT NULL default '0',
-		  `lag` int(11) NOT NULL default '0',
-		  PRIMARY KEY  (`id`)
-		) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=0;");
-        $db->execute();
-
-        $db->setQuery("CREATE TABLE IF NOT EXISTS `#__jupolls_options` (
-		  `id` int(11) NOT NULL auto_increment,
-		  `poll_id` int(11) NOT NULL default '0',
-		  `text` text NOT NULL,
-		  `link` varchar(255) DEFAULT NULL,
-		  `ordering` int(11) NOT NULL,
-		  PRIMARY KEY  (`id`),
-		  KEY `poll_id` (`poll_id`,`text`(1))
-		) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=0;");
-        $db->execute();
-
-        $db->setQuery("CREATE TABLE IF NOT EXISTS `#__jupolls_votes` (
-		  `id` bigint(20) NOT NULL auto_increment,
-		  `date` datetime NOT NULL default '0000-00-00 00:00:00',
-		  `option_id` int(11) NOT NULL default '0',
-		  `poll_id` int(11) NOT NULL default '0',
-		  `ip` int(10) unsigned NOT NULL,
-		  `browser` varchar(155) NOT NULL,
-		  `user_id` int(11) DEFAULT NULL,
-		  PRIMARY KEY  (`id`),
-		  KEY `poll_id` (`poll_id`)
-		) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=0;");
-        $db->execute();
 
         $installer = new JInstaller();
         $installer->install($src . '/extensions/mod_jupolls');
@@ -128,68 +119,40 @@ class com_JUPollsInstallerScript
         $installer = new JInstaller();
         $installer->install($src . '/extensions/plg_jupollssearch');
 
-        if($this->_is_new_installation == true)
+        ob_start();
+        $this->_installationOutput($status);
+        $html = ob_get_contents();
+        ob_end_clean();
+
+        $version = new JVersion;
+        $joomla  = substr($version->getShortVersion(), 0, 3);
+
+        if($joomla < '3.4')
         {
-            $this->_installJUPolls();
+            echo $html;
         }
         else
         {
-            $this->_updateJUPolls();
-
-            $query = $db->getQuery(true);
-            $query->select('*');
-            $query->from('#__extensions');
-            $query->where('`type` = "component"');
-            $query->where('`element` = "com_jupolls"');
-            $db->setQuery($query);
-            $manifests = $db->loadObjectList();
-
-            foreach ($manifests AS $manifest)
-            {
-                $array = json_decode($manifest->manifest_cache, true);
-            }
-
-            $version = $array['version'];
-
-            if(version_compare($version, '1.5.4', '<='))
-            {
-                $db = JFactory::getDbo();
-
-                $db->setQuery("ALTER TABLE `#__jupolls_votes` ENGINE=InnoDB");
-                $db->execute();
-
-                $db->setQuery("ALTER TABLE `#__jupolls_options` ENGINE=InnoDB");
-                $db->execute();
-
-                $db->setQuery("ALTER TABLE `#__jupolls_polls` ENGINE=InnoDB");
-                $db->execute();
-            }
+            $app->enqueueMessage($html, 'message');
         }
 
-        $this->_installationOutput($status);
-    }
-
-    protected function _installJUPolls()
-    {
-        if(empty($this->_current_version)) {
-            return;
-        }
-    }
-
-    protected function _updateJUPolls()
-    {
-        if(empty($this->_current_version)) {
-            return;
-        }
+        return true;
     }
 
     public function uninstall($parent)
     {
         $status = new JObject();
+        $app    = JFactory::getApplication();
 
         $db = JFactory::getDBO();
 
-        $db->setQuery("SELECT extension_id FROM #__extensions WHERE type = 'module' AND element = 'mod_jupolls' LIMIT 1");
+        $q_mod = $db->getQuery(true);
+        $q_mod
+            ->select('extension_id')
+            ->from('#__extensions')
+            ->where($db->quoteName('type') . ' = ' . $db->quote('module'))
+            ->where($db->quoteName('element') . ' = ' . $db->quote('mod_jupolls'));
+        $db->setQuery($q_mod, 0, 1);
         $id = $db->loadResult();
 
         if($id)
@@ -198,7 +161,14 @@ class com_JUPollsInstallerScript
             $installer->uninstall('module', $id);
         }
 
-        $db->setQuery("SELECT extension_id FROM #__extensions WHERE type = 'plugin' AND element = 'jupollssearch' AND folder = 'search' LIMIT 1");
+        $q_plg = $db->getQuery(true);
+        $q_plg
+            ->select('extension_id')
+            ->from('#__extensions')
+            ->where($db->quoteName('type') . ' = ' . $db->quote('plugin'))
+            ->where($db->quoteName('element') . ' = ' . $db->quote('plg_jupollssearch'))
+            ->where($db->quoteName('folder') . ' = ' . $db->quote('search'));
+        $db->setQuery($q_plg, 0, 1);
         $id = $db->loadResult();
 
         if($id)
@@ -207,7 +177,157 @@ class com_JUPollsInstallerScript
             $installer->uninstall('plugin', $id);
         }
 
+        ob_start();
         $this->_uninstallationOutput($status);
+        $html = ob_get_contents();
+        ob_end_clean();
+
+        $version = new JVersion;
+        $joomla  = substr($version->getShortVersion(), 0, 3);
+
+        if($joomla < '3.4')
+        {
+            echo $html;
+        }
+        else
+        {
+            $app->enqueueMessage($html, 'message');
+        }
+
+        return true;
+    }
+
+    protected function _installationOutput($status)
+    {
+        ?>
+        <style type="text/css">
+            .juinstall {
+                clear: both;
+                color: #333 !important;
+                font-weight: normal;
+                margin: 0 !important;
+                padding: 0;
+                background: #fff !important;
+                position: absolute !important;
+                top: 83px !important;
+                left: 0 !important;
+                overflow: hidden;
+                min-width: 100%;
+                max-width: 100%;
+                width: 100%;
+                height: 100%;
+                z-index: 100 !important;
+            }
+            .juinstall-content {
+                margin: 5% auto 8% auto !important;
+                padding: 0 0 18px 0;
+                width: 40%;
+            }
+            .juinstall hr {
+                margin-top: 6px;
+                margin-bottom: 6px;
+                border: 0;
+                border-top: 1px solid #eee
+            }
+        </style>
+        <div class="juinstall">
+            <div class="juinstall-content">
+                <h2>JUPolls: <?php echo JText::_('Installed'); ?></h2>
+                <p><a href="index.php?option=com_jupolls" class="btn btn-success">Go to JUPolls</a></p>
+                <hr>
+                <table class="table table-striped">
+                    <thead>
+                    <tr>
+                        <th><?php echo JText::_('Extension'); ?></th>
+                        <th><?php echo JText::_('Status'); ?></th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td><?php echo 'JUPolls ' . JText::_('Component'); ?></td>
+                        <td><span class="label label-success"><?php echo JText::_('Installed'); ?></span</td>
+                    </tr>
+                    <tr>
+                        <td><?php echo 'JUPolls ' . JText::_('Module'); ?></td>
+                        <td><span class="label label-success"><?php echo JText::_('Installed'); ?></span></td>
+                    </tr>
+                    <tr>
+                        <td><?php echo 'Search - JUPolls'; ?></td>
+                        <td><span class="label label-success"><?php echo JText::_('Installed'); ?></span></td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <?php
+
+        return;
+    }
+
+    private function _uninstallationOutput($status)
+    {
+        ?>
+        <style type="text/css">
+            .juinstall {
+                clear: both;
+                color: #333 !important;
+                font-weight: normal;
+                margin: 0 !important;
+                padding: 0;
+                background: #fff !important;
+                position: absolute !important;
+                top: 83px !important;
+                left: 0 !important;
+                overflow: hidden;
+                min-width: 100%;
+                max-width: 100%;
+                width: 100%;
+                height: 100%;
+                z-index: 100 !important;
+            }
+            .juinstall-content {
+                margin: 5% auto 8% auto !important;
+                padding: 0 0 18px 0;
+                width: 40%;
+            }
+            .juinstall hr {
+                margin-top: 6px;
+                margin-bottom: 6px;
+                border: 0;
+                border-top: 1px solid #eee
+            }
+        </style>
+        <div class="juinstall">
+            <div class="juinstall-content">
+                <h2>JUPolls: <?php echo JText::_('Removed'); ?></h2>
+                <hr>
+                <table class="table table-striped">
+                    <thead>
+                    <tr>
+                        <th><?php echo JText::_('Extension'); ?></th>
+                        <th><?php echo JText::_('Status'); ?></th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td><?php echo JText::_('Component'); ?></td>
+                        <td><span class="label label-success"><?php echo JText::_('Removed'); ?></span></td>
+                    </tr>
+                    <tr>
+                        <td><?php echo 'JUPolls ' . JText::_('Module'); ?></td>
+                        <td><span class="label label-success"><?php echo JText::_('Removed'); ?></span></td>
+                    </tr>
+                    <tr>
+                        <td><?php echo 'Search - JUPolls'; ?></td>
+                        <td><span class="label label-success"><?php echo JText::_('Removed'); ?></span></td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <?php
+
+        return;
     }
 
     public function unlinkRecursive($dir, $deleteRootToo)
@@ -237,87 +357,5 @@ class com_JUPollsInstallerScript
         }
 
         return;
-    }
-
-    protected function _installationOutput($status)
-    {
-        ?>
-        <h2>JUPolls Installation</h2>
-        <h2><a href="index.php?option=com_jupolls">Go to JUPolls</a></h2>
-        <table class="adminlist table table-striped">
-            <thead>
-            <tr>
-                <th class="title" colspan="2"><?php echo JText::_('Extension'); ?></th>
-                <th width="30%"><?php echo JText::_('Status'); ?></th>
-            </tr>
-            </thead>
-            <tfoot>
-            <tr>
-                <td colspan="3"></td>
-            </tr>
-            </tfoot>
-            <tbody>
-            <tr>
-                <th colspan="3"><?php echo JText::_('Core'); ?></th>
-            </tr>
-            <tr class="row0">
-                <td class="key" colspan="2"><?php echo 'JUPolls ' . JText::_('Component'); ?></td>
-                <td><strong><?php echo JText::_('Installed'); ?></strong></td>
-            </tr>
-            <tr class="row1">
-                <td class="key" colspan="2"><?php echo 'JUPolls ' . JText::_('Module'); ?></td>
-                <td><strong><?php echo JText::_('Installed'); ?></strong></td>
-            </tr>
-            <tr>
-                <th colspan="3"><?php echo JText::_('Plugins'); ?></th>
-            </tr>
-            <tr class="row1">
-                <td class="key" colspan="2"><?php echo 'Content - Load JUPolls'; ?></td>
-                <td><strong><?php echo JText::_('Installed'); ?></strong></td>
-            </tr>
-            <tr class="row0">
-                <td class="key" colspan="2"><?php echo 'Search - JUPolls'; ?></td>
-                <td><strong><?php echo JText::_('Installed'); ?></strong></td>
-            </tr>
-            </tbody>
-        </table>
-        <?php
-    }
-
-    private function _uninstallationOutput($status)
-    {
-        ?>
-        <h2>JUPolls Removal</h2>
-        <table class="adminlist table table-striped">
-            <thead>
-            <tr>
-                <th class="title" colspan="2"><?php echo JText::_('Extension'); ?></th>
-                <th width="30%"><?php echo JText::_('Status'); ?></th>
-            </tr>
-            </thead>
-            <tfoot>
-            <tr>
-                <td colspan="3"></td>
-            </tr>
-            </tfoot>
-            <tbody>
-            <tr>
-                <th colspan="3"><?php echo JText::_('Core'); ?></th>
-            </tr>
-            <tr class="row0">
-                <td class="key" colspan="2"><?php echo JText::_('Component'); ?></td>
-                <td><strong><?php echo JText::_('Removed'); ?></strong></td>
-            </tr>
-            <tr class="row1">
-                <td class="key" colspan="2"><?php echo 'JUPolls ' . JText::_('Module'); ?></td>
-                <td><strong><?php echo JText::_('Removed'); ?></strong></td>
-            </tr>
-            <tr class="row0">
-                <td class="key" colspan="2"><?php echo 'Search - JUPolls'; ?></td>
-                <td><strong><?php echo JText::_('Removed'); ?></strong></td>
-            </tr>
-            </tbody>
-        </table>
-        <?php
     }
 }
